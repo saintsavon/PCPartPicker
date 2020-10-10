a17fa754f41cedd08221ca35fc97736a90c6117f
saintsavon <jaylensavon@gmail.com>
2020-10-10 11:06:01 -0400

    Setting up for factory pattern

---
+++ PartPicker.java
@@ -0,0 +1,11 @@
+public interface PartPicker {
+    
+    public CPU chooseCPU();
+    public MOBO chooseMOBO();
+    public Cooler chooseCooler();
+	public Case chooseCase();
+	public RAM chooseRAM();
+	public GPU chooseGPU();
+    public PSU choosePSU();
+    
+}
