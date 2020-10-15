5fb4758df9899be556709eaf4f3106796cdd10d1
saintsavon <jaylensavon@gmail.com>
2020-10-15 15:29:53 -0400

    Messing with component storage options

---
+++ r5_3600X.java
@@ -0,0 +1,7 @@
+public class r5_3600X {
+    String name = "Ryzen 5 - 3600X";
+    String brand = "AMD";
+    String chipset = "AM4";
+    int powerConsumption = 95;
+    double cost = 209.09;
+}
---
+++ r7_3700X.java
@@ -0,0 +1,7 @@
+public class r7_3700X {
+    String name = "Ryzen 7 - 3700X";
+    String brand = "AMD";
+    String chipset = "AM4";
+    int powerConsumption = 65;
+    double cost = 294.99;
+}
