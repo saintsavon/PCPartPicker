fccb1e8855b1e2e2d6c329e5330d1e1b5786d0df
saintsavon <jaylensavon@gmail.com>
2020-10-15 17:42:02 -0400

    Removing bloat files

--- PartPickerMain.java
+++ PartPickerMain.java
@@ -2,13 +2,10 @@ import java.util.*;
 
 public class PartPickerMain {
 
-    String cpu = "";
-
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Please select your CPU Brand (Intel or AMD)?");
+        System.out.println("Please select your CPU Brand (intel or amd)?");
         String cpu = scan.nextLine();
 
     }
